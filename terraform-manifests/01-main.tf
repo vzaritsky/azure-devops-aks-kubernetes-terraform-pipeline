c2550ccad8daa723ff9574fc1430bcde0c126bdf
Aleks <aleksandr.tolotnev@gmail.com>
2021-08-31 12:07:11 +0300

    Start 4.3

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -9,7 +9,7 @@
 # 1. Terraform Settings Block
 terraform {
   # 1. Required Version Terraform
-  required_version = ">= 0.13"
+  # required_version = ">= 0.13"
   # 2. Required Terraform Providers  
   required_providers {
     azurerm = {
@@ -26,7 +26,7 @@ terraform {
     }
   }
 
-# Terraform State Storage to Azure Storage Container
+ # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     #resource_group_name   = "terraform-storage-rg"
     #storage_account_name  = "terraformstatexlrwdrzs"
